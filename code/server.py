37bdb9bafb0238f37638dda9bbd6407a2a334416
FaizAther <faizather@protonmail.com>
2021-08-06 11:07:19 +1000

    boiler plate

--- code/server.py
+++ code/server.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, send_from_directory
+import os
+
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for("hello_world", name="admin"))
+
+@app.route('/hello/<name>')
+def hello_world(name):
+    return hello(name)
+
+def hello(name):
+    return render_template("hello.html", content=f"Hello {name}")
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
+
