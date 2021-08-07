5449c6cd95d3e5832cc8341c8c081569e40bf6f6
FaizAther <faizather@protonmail.com>
2021-08-07 10:40:32 +1000

    db stuff

--- code/server.py
+++ code/server.py
@@ -6,13 +6,23 @@ from flask import (
 )
 from flask_sqlalchemy import SQLAlchemy
 
+DATABASE_NAME = 'gpr.db'
+
 app = Flask(__name__)
 app.secret_key = "gpr"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DATABASE_NAME
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.id}, {self.username}')"
+
 
 @app.route('/favicon.ico')
 def favicon():
@@ -27,13 +37,25 @@ def index():
 def admin():
     return redirect(url_for("hello_world", name="admin"))
 
+@app.route('/show/<name>')
+def show_hello(name):
+    id = User.query.filter_by(username=name).first()
+    return render_template("hello.html", content=f"Hello name={name}, id={id}")
+
 @app.route('/hello/<name>')
 def hello_world(name):
+    add_user(name)
     return hello(name)
+
+def add_user(name):
+    db.session.add(User(id=len(User.query.all()), username=name))
+    db.session.commit()
 
 def hello(name):
     return render_template("hello.html", content=f"Hello {name}")
 
 if __name__ == '__main__':
+    if DATABASE_NAME not in os.listdir():
+        db.create_all()
     app.debug = True
     app.run()
