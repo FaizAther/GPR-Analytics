ef65b9ebfea8054cca9005c00299fcaf4d6f03ae
FaizAther <faizather@protonmail.com>
2021-08-29 10:50:07 +1000

    more classes

--- code/Event.py
+++ code/Event.py
@@ -8,11 +8,12 @@ class Event(IdClass):
     def __init__(self, id):
         super().__init__(id)
         self._date_duration = {datetime.now : 0}
-        self._manager = None
-        self._organizers = []
-        self._guests = []
-        self._invitees = []
-        self._attendance = []
-        self._locations = []
-        self._resources = []
+        self._manager       = None
+        self._organizers    = []
+        self._guests        = []
+        self._invitees      = []
+        self._attendance    = []
+        self._locations     = []
+        self._resources     = []
+        self._weighting     = 0
         self._type = EventType.DEFAULT
