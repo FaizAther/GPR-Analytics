818a8ca36fab80249a3449d7e5422bba4ba44af7
FaizAther <faizather@protonmail.com>
2021-08-29 10:48:19 +1000

    more classes

--- code/Event.py
+++ code/Event.py
@@ -1,4 +1,5 @@
 from IdClass import IdClass
+from EventType import EventType
 from datetime import datetime
 '''
     Event Class
@@ -6,7 +7,7 @@ from datetime import datetime
 class Event(IdClass):
     def __init__(self, id):
         super().__init__(id)
-        self._date_duration = {}
+        self._date_duration = {datetime.now : 0}
         self._manager = None
         self._organizers = []
         self._guests = []
@@ -14,3 +15,4 @@ class Event(IdClass):
         self._attendance = []
         self._locations = []
         self._resources = []
+        self._type = EventType.DEFAULT
---
+++ code/EventType.py
@@ -0,0 +1,12 @@
+from enum import Enum
+'''
+    Event Type
+'''
+class EventType(Enum):
+    LECTURE         = 0
+    TUTORIAL        = 1
+    PRACTICAL       = 2
+    CONSULTATION    = 3
+    GUEST           = 4
+    EXAM            = 5
+    DEFAULT         = 6
