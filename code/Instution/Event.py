0bce1111b71ae9f3f016226b7d50fea75c933f0b
FaizAther <faizather@protonmail.com>
2021-08-30 12:29:35 +1000

    clean

--- code/Instution/Event.py
+++ code/Instution/Event.py
@@ -1,6 +1,7 @@
-from User       import User
-from EventType  import EventType
 from Base       import Base
+from User       import User
+from Location   import Location
+from EventType  import EventType
 from datetime   import datetime
 '''
     Event Class
@@ -8,12 +9,12 @@ from datetime   import datetime
 class Event(Base):
     def __init__(self, id, type):
         super().__init__(id)
-        self._date_duration :dict       = {datetime.now : 0}
-        self._manager       :User       = None
-        self._organizers    :list[User] = list[User]
-        self._guests        :list[User] = list[User]
-        self._invitees      :list[User] = list[User]
-        self._attendance    :list[User] = list[User]
-        self._locations     :list[User] = list[User]
-        self._resources     :list[User] = list[User]
-        self._type          :EventType  = type
+        self._date_duration :dict           = {datetime.now : 0}
+        self._manager       :User           = None
+        self._organizers    :list[User]     = list[User]
+        self._guests        :list[User]     = list[User]
+        self._invitees      :list[User]     = list[User]
+        self._attendance    :list[User]     = list[User]
+        self._locations     :list[Location] = list[Location]
+        self._resources     :list           = list
+        self._type          :EventType      = type
