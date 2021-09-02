0a946b8d789fadcc489c261f9255005f036bae28
FaizAther <faizather@protonmail.com>
2021-09-02 12:39:14 +1000

    moving

--- code/UniQLD.py
+++ code/UniQLD.py
@@ -25,11 +25,12 @@ t0 = Tutor(300)
 
 l0 = Lecturer(400)
 f0 = Faculty(0, name="COMP")
+uni0.add_faculty(f0)
 f0.make_course(3301)
 f0.add_user(u0, 3301)
 f0.add_user(l0, 3301)
 
-print("admin is: ", Base.dict_find(3301, f0.get_courses()).get_admin())
+#print("admin is: ", Base.dict_find(3301, f0.get_courses()).get_admin())
 
 #print(c0)
 f0.add_users([u1,u2], 3301)
@@ -46,6 +47,6 @@ e0.set_weighting(5)
 
 e0.add_users([l0,t0,u0,u1,u2,u3])
 
-print(t0.get_capacity())
+#print(t0.get_capacity())
 
 #print(u0)
