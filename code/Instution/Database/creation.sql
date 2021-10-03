8b8ed83e19c1bc41be3806121685c9f774215fef
FaizAther <faizather@protonmail.com>
2021-10-03 12:12:46 +1000

    notify

--- code/Instution/Database/creation.sql
+++ code/Instution/Database/creation.sql
@@ -31,19 +31,36 @@ CREATE TABLE Course (
 
 CREATE TABLE Event (
     id numeric PRIMARY KEY,
+    course_id numeric NOT NULL,
     name text NOT NULL,
-    manager numeric NOT NULL, 
-    foreign key (manager) references User(id)
+    manager numeric NOT NULL,
+    foreign key (manager) references User(id),
+    foreign key (course_id) references Course(id)
 );
 
 CREATE TABLE Attendance (
     id numeric PRIMARY KEY,
+    event_id numeric,
     name text NOT NULL,
-    authority numeric NOT NULL,
-    foreign key (authority) references User(id)
+    marker_id numeric NOT NULL,
+    student_id numeric NOT NULL,
+    startdate date NOT NULL,
+    foreign key (event_id) references Event(id),
+    foreign key (marker_id) references User(id),
+    foreign key (student_id) references User(id)
 );
 
-CREATE TABLE EventTicket (
+CREATE TABLE Mark (
+    attendance_id numeric,
+    total numeric, 
+    received numeric,
+    spec text,
+    duedate date,
+    foreign key (attendance_id) references Attendance(id)
+    primary key (attendance_id, duedate)
+);
+
+CREATE TABLE Invite (
     event_id numeric NOT NULL,
     user_id numeric NOT NULL,
     foreign key (event_id) references Event(id),
