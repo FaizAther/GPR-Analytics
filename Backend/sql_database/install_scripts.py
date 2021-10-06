655abe2c87fc8d8113c628339b41cd8bd6b3b077
Jono <jonathan_chai90@hotmail.com>
2021-10-06 12:19:45 +1000

    Wrong branch. Revert "Added Backend and Frontend folders. Frontend folder will be a collection of pages."
    This reverts commit 3c2670035fe6d7cb9a57ddc18478aba1b9a51af8.

--- Backend/sql_database/install_scripts.py
+++
@@ -1,7 +0,0 @@
-# for database.py and query.py
-from pip._internal import main
-main(['install','mysql-connector-python-rf'])
-
-# for populate.py
-from pip._internal import main
-main(['install','names'])
--- Backend/sql_database/query.py
+++
@@ -1,9 +0,0 @@
-# WRITE YOUR QUERY IN THE query VARIABLE #
-
-from database import query_data
-def write_query():
-    query = "SELECT * FROM student"
-    return query
-
-if __name__ == "__main__":
-    query_data(write_query())
