89133ae976deae9ead21416f4c45302dabf8fca5
Jono <jonathan_chai90@hotmail.com>
2021-10-19 14:36:30 +1000

    added sql folder

---
+++ Backend/sql_database/query.py
@@ -0,0 +1,9 @@
+# WRITE YOUR QUERY IN THE query VARIABLE #
+
+from database import query_data
+def write_query():
+    query = "SELECT * FROM student"
+    return query
+
+if __name__ == "__main__":
+    query_data(write_query())
