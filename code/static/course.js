c0c2dba1c6d0509dbc6fadcd6a12f37395686be0
Jonathan Chai <Jono>
2021-10-24 16:38:29 +1000

    added dark mode functionality.

---
+++ code/static/course.js
@@ -0,0 +1,13 @@
+// DARK MODE
+function darkmode() {
+    const wasDarkmode = localStorage.getItem('darkmode') === 'true';
+    localStorage.setItem('darkmode', !wasDarkmode);
+    const element = document.body;
+    element.classList.toggle('dark-mode', !wasDarkmode);
+}
+
+function onload() {
+document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
+}
+
+// SETTINGS TEXT REVEAL FOR NOTIFICATION SETTING SAVE
