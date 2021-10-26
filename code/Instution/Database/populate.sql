5d09b199d9f2efbc5185dbb3efe6384aabb034bf
FaizAther <faizather@protonmail.com>
2021-10-26 16:29:29 +1000

    data

--- code/Instution/Database/populate.sql
+++ code/Instution/Database/populate.sql
@@ -119,18 +119,19 @@ INSERT INTO Enrollment (user_id, course_id) VALUES (19, 11);
 
 -- Add Announcements
 
-INSERT INTO Announcement (course_id, position, created, description) VALUES (1, 0,2021-10-26 16:18:20.730264, "Welcome to week 1!"
-INSERT INTO Announcement (course_id, position, created, description) VALUES (2, 0,2021-10-26 16:18:20.730341, "Welcome to week 1!"
-INSERT INTO Announcement (course_id, position, created, description) VALUES (3, 0,2021-10-26 16:18:20.730351, "Welcome to week 1!"
-INSERT INTO Announcement (course_id, position, created, description) VALUES (4, 0,2021-10-26 16:18:20.730359, "Welcome to week 1!"
-INSERT INTO Announcement (course_id, position, created, description) VALUES (5, 0,2021-10-26 16:18:20.730365, "Welcome to week 1!"
-INSERT INTO Announcement (course_id, position, created, description) VALUES (6, 0,2021-10-26 16:18:20.730372, "Welcome to week 1!"
-INSERT INTO Announcement (course_id, position, created, description) VALUES (7, 0,2021-10-26 16:18:20.730378, "Welcome to week 1!"
-INSERT INTO Announcement (course_id, position, created, description) VALUES (8, 0,2021-10-26 16:18:20.730384, "Welcome to week 1!"
-INSERT INTO Announcement (course_id, position, created, description) VALUES (9, 0,2021-10-26 16:18:20.730390, "Welcome to week 1!"
-INSERT INTO Announcement (course_id, position, created, description) VALUES (10, 0,2021-10-26 16:18:20.730396, "Welcome to week 1!"
-INSERT INTO Announcement (course_id, position, created, description) VALUES (11, 0,2021-10-26 16:18:20.730403, "Welcome to week 1!"
-INSERT INTO Announcement (course_id, position, created, description) VALUES (12, 0,2021-10-26 16:18:20.730409, "Welcome to week 1!"
+INSERT INTO Announcement (course_id, position, created, description) VALUES (1, 0, "2021-10-26 16:28:17.303301", "Welcome to week 1!");
+INSERT INTO Announcement (course_id, position, created, description) VALUES (2, 0, "2021-10-26 16:28:17.303333", "Welcome to week 1!");
+INSERT INTO Announcement (course_id, position, created, description) VALUES (3, 0, "2021-10-26 16:28:17.303338", "Welcome to week 1!");
+INSERT INTO Announcement (course_id, position, created, description) VALUES (4, 0, "2021-10-26 16:28:17.303341", "Welcome to week 1!");
+INSERT INTO Announcement (course_id, position, created, description) VALUES (5, 0, "2021-10-26 16:28:17.303345", "Welcome to week 1!");
+INSERT INTO Announcement (course_id, position, created, description) VALUES (6, 0, "2021-10-26 16:28:17.303348", "Welcome to week 1!");
+INSERT INTO Announcement (course_id, position, created, description) VALUES (7, 0, "2021-10-26 16:28:17.303351", "Welcome to week 1!");
+INSERT INTO Announcement (course_id, position, created, description) VALUES (8, 0, "2021-10-26 16:28:17.303354", "Welcome to week 1!");
+INSERT INTO Announcement (course_id, position, created, description) VALUES (9, 0, "2021-10-26 16:28:17.303358", "Welcome to week 1!");
+INSERT INTO Announcement (course_id, position, created, description) VALUES (10, 0, "2021-10-26 16:28:17.303365", "Welcome to week 1!");
+INSERT INTO Announcement (course_id, position, created, description) VALUES (11, 0, "2021-10-26 16:28:17.303368", "Welcome to week 1!");
+INSERT INTO Announcement (course_id, position, created, description) VALUES (12, 0, "2021-10-26 16:28:17.303371", "Welcome to week 1!");
+
 
 -- Add Events
 
