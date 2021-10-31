d7fd9a1db1a1ef44c953fde374fa389e36e3a74f
FaizAther <faizather@protonmail.com>
2021-10-31 17:20:04 +1000

    python path

--- code/Instution/Database/make_database.sh
+++ code/Instution/Database/make_database.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+export PYTHONPATH=`pwd | sed 's/\/Instution\/Database$//'`
+echo "export PYTHONPATH=$PYTHONPATH"
 rm -rf gpr.db
 sqlite3 gpr.db < creation.sql
 sqlite3 gpr.db < populate.sql
 sqlite3 gpr.db < events.sql
 cp gpr.db ../
 cp gpr.db ../../
+rm -rf $PYTHONPATH/uploads
 mkdir $PYTHONPATH/uploads
